db1b0071613c20d107c0702ff49b3c105bf50eaf
Lukasz Kreczko <lkreczko@googlemail.com>
2018-01-02 17:17:20 +0100

    code formatting for app.py

--- app.py
+++ app.py
@@ -8,19 +8,20 @@ import confluent_kafka
 app = Flask(__name__)
 KAFKA_PRODUCER = None
 
+
 def get_kafka_producer():
     '''
         Get the kafka producer, might differ depending on service
     '''
     global KAFKA_PRODUCER
     conf = {
-            'bootstrap.servers': os.environ['CLOUDKARAFKA_BROKERS'],
-            'session.timeout.ms': 6000,
-            'default.topic.config': {'auto.offset.reset': 'smallest'},
-            'security.protocol': 'SASL_SSL',
-            'sasl.mechanisms': 'SCRAM-SHA-256',
-            'sasl.username': os.environ['CLOUDKARAFKA_USERNAME'],
-            'sasl.password': os.environ['CLOUDKARAFKA_PASSWORD'],
+        'bootstrap.servers': os.environ['CLOUDKARAFKA_BROKERS'],
+        'session.timeout.ms': 6000,
+        'default.topic.config': {'auto.offset.reset': 'smallest'},
+        'security.protocol': 'SASL_SSL',
+        'sasl.mechanisms': 'SCRAM-SHA-256',
+        'sasl.username': os.environ['CLOUDKARAFKA_USERNAME'],
+        'sasl.password': os.environ['CLOUDKARAFKA_PASSWORD'],
     }
     if KAFKA_PRODUCER is not None:
         return KAFKA_PRODUCER
@@ -30,6 +31,7 @@ def get_kafka_producer():
     except Exception as e:
         print('Could not create kafka producer', e)
         raise e
+
 
 @app.route('/gitlab_forwarder', methods=['POST'])
 def forward_gitlab():
@@ -49,6 +51,7 @@ def forward_gitlab():
 
     return "OK"
 
+
 @app.route('/')
 def homepage():
 
@@ -58,6 +61,7 @@ def homepage():
 
     <img src="http://loremflickr.com/600/400">
     """.format(time="too early")
+
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
