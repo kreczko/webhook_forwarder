dc2407b4c79e42df804901a929d7be1d63d5a7ef
Lukasz Kreczko <lkreczko@googlemail.com>
2018-01-02 19:49:36 +0100

    tidying up code

--- app.py
+++ app.py
@@ -2,35 +2,9 @@ import os
 import json
 import sys
 from flask import Flask, abort, request
-# from kafka_helper import get_kafka_producer
-import confluent_kafka
+from kafka_helper import get_kafka_producer
 
 app = Flask(__name__)
-KAFKA_PRODUCER = None
-
-
-def get_kafka_producer():
-    '''
-        Get the kafka producer, might differ depending on service
-    '''
-    global KAFKA_PRODUCER
-    conf = {
-        'bootstrap.servers': os.environ['CLOUDKARAFKA_BROKERS'],
-        'session.timeout.ms': 6000,
-        'default.topic.config': {'auto.offset.reset': 'smallest'},
-        'security.protocol': 'SASL_SSL',
-        'sasl.mechanisms': 'SCRAM-SHA-256',
-        'sasl.username': os.environ['CLOUDKARAFKA_USERNAME'],
-        'sasl.password': os.environ['CLOUDKARAFKA_PASSWORD'],
-    }
-    if KAFKA_PRODUCER is not None:
-        return KAFKA_PRODUCER
-    try:
-        KAFKA_PRODUCER = confluent_kafka.Producer(**conf)
-        return KAFKA_PRODUCER
-    except Exception as e:
-        print('Could not create kafka producer', e)
-        raise e
 
 
 @app.route('/gitlab_forwarder', methods=['POST'])
