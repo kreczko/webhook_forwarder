9903f212f5fb403f4e69bed3fee570d5fe48483d
kreczko <lkreczko@googlemail.com>
2018-05-08 16:44:42 +0100

    added logging

--- app.py
+++ app.py
@@ -3,19 +3,27 @@ import json
 import sys
 from flask import Flask, abort, request
 from kafka_helper import get_kafka_producer
+import logging
 
 app = Flask(__name__)
-
+app.logger.addHandler(logging.StreamHandler(sys.stdout))
+app.logger.setLevel(logging.ERROR)
+if os.environ.get('DEBUG', False):
+    app.logger.setLevel(logging.DEBUG)
 
 @app.route('/gitlab_forwarder', methods=['POST'])
 def forward_gitlab():
     kafka_topic = "{}".format(os.environ['KAFKA_TOPIC'])
+    debug = os.environ.get('DEBUG', False)
+    app.logger.debug('received a message ')
 
     TOKEN = os.environ.get("GITLAB_TOKEN")
     if TOKEN is None or request.headers.get('X-Gitlab-Token') != TOKEN:
         abort(401)
+        app.logger.debug('This message is INVALID')
         return
 
+    app.logger.debug('This is a valid message')
     content = request.get_json(silent=True)
     js = json.dumps(content)
 
